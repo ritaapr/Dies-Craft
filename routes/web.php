19c41b253186cac56ee5fd8be01ded2ba8db895b
ritaapr <aprilritaapr@gmail.com>
2024-11-14 20:52:34 +0700

    frontend login admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\AdminLoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/login',[AdminLoginController::class,'index'])->name('admin.login');
